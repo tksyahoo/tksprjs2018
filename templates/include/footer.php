7d9cca78d34a628ac8d8af036e334e780503bf9e
TKS <tapan.s81@gmail.com>
2018-07-12 17:43:54 +0530

    initial commit

---
+++ templates/include/footer.php
@@ -0,0 +1,17 @@
+      <!-- <div id="footer">
+        Widget News &copy; 2011. All rights reserved. <a href="admin.php">Site Admin</a>
+      </div>
+
+    </div> -->
+    Widget News &copy; 2011. All rights reserved. <a href="admin.php">Site Admin</a>
+<script type="text/javascript" href="../../cms/new/vendor/jquery/jquery.js"></script>
+    <script type="text/javascript">
+    	// $(document).ready(function(){
+    	// 	$(".card").eq(0);return;
+    	// 	$(".card").eq(0).css('margin-top',50);
+    	// });
+    	
+    </script>
+  </body>
+</html>
+
