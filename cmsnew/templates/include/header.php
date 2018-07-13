04c853ab56ef069057c3c790d679507da4ff5f46
TKS <tapan.s81@gmail.com>
2018-07-13 18:04:25 +0530

    menu added

--- cmsnew/templates/include/header.php
+++ cmsnew/templates/include/header.php
@@ -19,7 +19,7 @@
   </head>
 
   <body>
-   <!--  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
         <a class="navbar-brand" href="../../cms/cmsnew/">My Blogs</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,4 +36,4 @@
           </ul>
         </div>
       </div>
-    </nav> -->
+    </nav>
